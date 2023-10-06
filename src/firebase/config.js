fb11833d6ca2edd54a6b6ad639275e1af6dd1ada
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-10-06 23:28:21 +0100

    added config files for backend services that will be used

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyClBbz4smn71XitRl07dKZOkcMnKaHx5GI",
+    authDomain: "book-4cec8.firebaseapp.com",
+    projectId: "book-4cec8",
+    storageBucket: "book-4cec8.appspot.com",
+    messagingSenderId: "599730761719",
+    appId: "1:599730761719:web:445f16c541841b332588f2"
+  };
+
+
+ //this is to init firebase (connect to backend)
+
+  firebase.initializeApp(firebaseConfig)
+
+  //this is to init services from firebase
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  //timestamp
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export { projectFirestore, projectAuth, timestamp }
