cb046c9f339a201f0d91569529033da58c284240
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-10-17 07:43:20 +0100

    added navbar router link to home if clicked

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
-
-
 <template>
-  
+  <Navbar />
+  <div>
+    <router-link to="/">Home</router-link> I <router-link to="/login">login</router-link> I
+    <router-link to="/signup">Signup</router-link>
 
-    <div>
-
-        <router-link to="/">Home</router-link> I
-        <router-link to="/login">login</router-link> I
-        <router-link to="/signup">Signup</router-link>
-
-        <router-view></router-view>
-
-    </div>
-  
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 
-
+export default {
+  components: { Navbar }
+}
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  
+
+   <div class="navbar">
+        <nav>
+            <h1>
+                <router-link :to="{ name: 'home'}">352 book</router-link>
+            </h1>
+        </nav>
+
+   </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
