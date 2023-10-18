6d25241ee144af160ed9ebe83e4aa3d0db98432c
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-10-18 21:54:01 +0100

    more tweaks to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,12 @@
 
    <div class="navbar">
         <nav>
-            <h1>
-                <router-link :to="{ name: 'home'}">352 book</router-link>
-            </h1>
+            <h1><router-link :to="{ name: 'home'}">352 book</router-link></h1>
+            <div class="links">
+                <button>Logout</button>
+                <router-link class="btn" to="/signup">Signup</router-link>
+                <router-link class="btn" to="/login"> Login</router-link>
+            </div>
         </nav>
 
    </div>
