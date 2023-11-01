3ba7fe70a767ec6181b14e0542cd280e6bc0c8a3
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-11-01 20:36:42 +0000

    added form fields to booklist comp. Will get more feedback for others before finishing it

--- src/views/booklist/CreateBooklist.vue
+++ src/views/booklist/CreateBooklist.vue
@@ -1,10 +1,21 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="home">
-     Create Booklist
-     
-    </div>
-    
+     <form  class="booklist">
+        <h4>Add a book</h4>
+        
+        <input type="text"  required placeholder="Book title..." v-model="booktitle">
+        <input type="text" placeholder="Book author..." v-model="bookauthor">
+        <input type="text" placeholder="Book Genre..." v-model="bookgenre">
+        <input type="numeric" placeholder="Number of pages" v-model="pagenumber">
+        <input type="numeric" placeholder="Rating out of 10..." v-model="ratingvalue">
+        <input type="boolean" placeholder="Did you finish...?" v-model="bookfinished">
+        <div v-if="error" class="error"> {{ error }}</div>
+        
+        <button v-if="!isPending">Submit</button>
+        <button v-if="isPending" disabled>Loading...</button>
+
+        
+    </form>
     
   </template>
   
@@ -20,4 +31,4 @@
       
   }
   </script>
-  
+
