80704612af21d4d54f9be0109c04dd72790117e0
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-11-02 22:14:21 +0000

    added refs for the data captured by the form, styled the form a little

--- src/views/booklist/CreateBooklist.vue
+++ src/views/booklist/CreateBooklist.vue
@@ -1,18 +1,23 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-     <form  class="booklist">
+     <form @submit.prevent="handleSubmit" class="booklist">
         <h4>Add a book</h4>
         
         <input type="text"  required placeholder="Book title..." v-model="booktitle">
         <input type="text" placeholder="Book author..." v-model="bookauthor">
         <input type="text" placeholder="Book Genre..." v-model="bookgenre">
-        <input type="numeric" placeholder="Number of pages" v-model="pagenumber">
+        <input type="numeric" placeholder="Number of pages" v-model="numberofpages">
         <input type="numeric" placeholder="Rating out of 10..." v-model="ratingvalue">
-        <input type="boolean" placeholder="Did you finish...?" v-model="bookfinished">
-        <div v-if="error" class="error"> {{ error }}</div>
+        <input type="boolean" placeholder="Did you finish the book (yes or no)?" v-model="bookfinished">
+        <textarea placeholder="100 word or less review" v-model="bookreview"></textarea>
+
+        <label>Upload book jacket</label>
+        <input type="file">
         
+        <div v-if="error" class="error"> {{ error }}</div>
         <button v-if="!isPending">Submit</button>
         <button v-if="isPending" disabled>Loading...</button>
+        
 
         
     </form>
@@ -21,14 +26,48 @@
   
   <script>
  
+ import { ref } from "vue";
+
   export default {
-      name: 'createbooklist',
+      
       
       setup() {
-         
-          return ;
+
+        const isPending = ref(false)
+        const error = ref(null)
+
+        const booktitle = ref('')
+        const bookauthor = ref('')
+        const bookgenre = ref('')
+        const numberofpages = ref('')
+        const ratingvalue = ref('')
+        const bookfinished = ref('')
+        const bookreview = ref('')
+
+        const handleSubmit = () => {
+            console.log(booktitle.value, bookauthor.value)
+        }
+
+          return {booktitle, bookauthor, bookgenre, numberofpages, ratingvalue, bookfinished, bookreview, isPending, error, handleSubmit} ;
       },
       
   }
   </script>
 
+<style>
+    input[type="file"] {
+        border: 0;
+        padding: 0;
+    }
+    label {
+        font-size: 14px;
+        font-weight: bold;
+        display: block;
+        margin-top: 30px
+    }
+    button {
+        margin-top: 20px;
+        margin-bottom: 20px
+    }
+    
+</style>
